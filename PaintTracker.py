45399e8feb2c608d7d70a44a4d3ce44a9ddd90f2
Yiminxue Zheng <yiminxuezheng@gmail.com>
2018-10-28 17:14:33 -0400

    Working with basic functions

--- PaintTracker.py
+++ PaintTracker.py
@@ -2,16 +2,24 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 import csv
 from kivy.properties import ListProperty
+from kivy.properties import NumericProperty
 
 
 class ColoursBox(BoxLayout):
     colour = ListProperty([0,0,0])
+    index = NumericProperty()
 
-    def __init__(self,name,company,colour,owned):
+    def __init__(self,index_id,name,company,colour,owned):
         BoxLayout.__init__(self)
+        self.index = index_id
         self.ids.name.text = name
         self.ids.company.text = company
         self.colour = self.hex_to_rgb(colour)
+
+        if owned == 'Y':
+            self.ids.owned.state ='down'
+        elif owned == 'N':
+            self.ids.unowned.state ='down'
 
 
 
@@ -22,6 +30,8 @@ class ColoursBox(BoxLayout):
 
 
 class PaintTrackerApp(App):
+
+    data = ListProperty(None)
 
     def load_data(self):
         self.data = []
@@ -37,16 +47,34 @@ class PaintTrackerApp(App):
                     else:
                         self.data.append(row)
 
+    def save_data(self):
+        with open('PaintList.csv','w') as csvfile:
+                writer = csv.writer(csvfile)
+
+                writer.writerow(["Company", "Paint Name", "HEX Code", "Owned"])
+
+                for row in self.data:
+                    writer.writerow(row)
 
     def build(self):
         root = self.root
 
         self.load_data()
 
-        data = self.data
+        for c in range(len(self.data)):
+            root.ids.grid.add_widget(ColoursBox(c,
+                                                self.data[c][0],
+                                                self.data[c][1],
+                                                self.data[c][2],
+                                                self.data[c][3]))
 
-        for c in range(len(data)):
-            root.ids.grid.add_widget(ColoursBox(data[c][0],  data[c][1],data[c][2], data[c][3]))
+    def update_ownership(self, owned_state,index,instance):
+
+        if owned_state == self.data[index][3]:
+            instance.state = 'down'
+        else:
+            self.data[index][3] = owned_state
+
 
 
 if __name__ == '__main__':
